0f2d8e454798cb2ba13284ebe88979106553364b
YanjunYu-1 <y9y9j8@163.com>
2022-05-11 06:11:28 -0500

    建立TicketNotifications与Tickets之间的关系

--- FinalByMyself/Models/TicketNotifications.cs
+++ FinalByMyself/Models/TicketNotifications.cs
@@ -3,7 +3,10 @@
     public class TicketNotifications//通知
     {
         public int Id { get; set; }
-        public int TicketId { get; set; }
         public string UserId { get; set; }
+
+        //TicketNotifications与Tickets  many to one
+        public int TicketId { get; set; }
+        public Tickets Tickets { get; set; }
     }
 }
