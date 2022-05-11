7705b5126164e5dd90fd92b1735a891edf614645
YanjunYu-1 <y9y9j8@163.com>
2022-05-11 06:08:58 -0500

    创建真正的TicketNotifications

---
+++ FinalByMyself/Models/TicketNotifications.cs
@@ -0,0 +1,9 @@
+﻿namespace FinalByMyself.Models
+{
+    public class TicketNotifications//通知
+    {
+        public int Id { get; set; }
+        public int TicketId { get; set; }
+        public string UserId { get; set; }
+    }
+}
