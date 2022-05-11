8da9719beee6e9d4e6c3bd3973fa1a20a9f679fa
YanjunYu-1 <y9y9j8@163.com>
2022-05-11 05:31:43 -0500

    创建Tickets基本属性

--- FinalByMyself/Models/Tickets.cs
+++ FinalByMyself/Models/Tickets.cs
@@ -2,6 +2,16 @@
 {
     public class Tickets
     {
-
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public int ProjectId { get; set; }
+        public int TicketTypeId { get; set; }
+        public int TicketPriorityId { get; set; }//优先级
+        public int TicketStatusId { get; set; }//状态
+        public string OwnerUserId { get; set; }//所有者
+        public string AssignedToUserId { get; set; }//分配给用户
     }
 }
