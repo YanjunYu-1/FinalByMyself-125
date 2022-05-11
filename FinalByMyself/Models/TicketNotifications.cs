a5bc0a1ade85099b0090b0ddead4dda158a502f3
YanjunYu-1 <y9y9j8@163.com>
2022-05-11 05:57:58 -0500

    创建TicketNotifications

---
+++ FinalByMyself/Models/TicketNotifications.cs
@@ -0,0 +1,13 @@
+﻿namespace FinalByMyself.Models
+{
+    public class TicketNotifications
+    {
+        public int Id { get; set; }
+        public int TicketId { get; set; }
+        public string Property { get; set; }//属性，特性
+        public string OldValue { get; set; }
+        public string NewValue { get; set; }
+        public DateTime Changed { get; set; }
+        public string UserId { get; set; }
+    }
+}
--- FinalByMyself/Models/Tickets.cs
+++ FinalByMyself/Models/Tickets.cs
@@ -10,7 +10,7 @@
         public int ProjectId { get; set; }
         public int TicketTypeId { get; set; }
         public int TicketPriorityId { get; set; }//优先级
-        //Q1 为什么有的是int有的是string
+        //Q1 为什么有的是int有的是string=>UserId需要手动编写，前面会添加部门等信息例如XSK001（销售科001）
         public int TicketStatusId { get; set; }//状态
         public string OwnerUserId { get; set; }//所有者
         public string AssignedToUserId { get; set; }//分配给用户
