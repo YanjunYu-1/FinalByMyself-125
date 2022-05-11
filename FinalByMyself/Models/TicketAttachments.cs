eec2fbd823479b7fc6f76bac5b69b1474bae5db5
YanjunYu-1 <y9y9j8@163.com>
2022-05-11 05:31:10 -0500

    创建TicketAttachments

--- FinalByMyself/Models/TicketAttachments.cs
+++ FinalByMyself/Models/TicketAttachments.cs
@@ -2,6 +2,12 @@
 {
     public class TicketAttachments
     {
-
+        public int Id { get; set; }
+        public string TicketId { get; set; }
+        public string FilePath { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public string UserId { get; set; }
+        public string FileUrl { get; set; }
     }
 }
