d4b3eff8755850c6674b0c5a426b61cb6165a384
YanjunYu-1 <y9y9j8@163.com>
2022-05-11 06:16:04 -0500

    创建ProjectsUsers

---
+++ FinalByMyself/Models/ProjectsUsers.cs
@@ -0,0 +1,9 @@
+﻿namespace FinalByMyself.Models
+{
+    public class ProjectsUsers
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int ProjectId { get; set; }
+    }
+}
